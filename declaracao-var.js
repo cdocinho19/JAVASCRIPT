7cd19dc364ed03892755a8a0036244a9b5ce1478
brenda <cdocinho19@gmail.com>
2026-09-22 18:16:16 -0300

    add declaracao-var

---
+++ declaracao-var.js
@@ -0,0 +1,13 @@
+function exemploVar() {
+    console.log(x);
+    
+    
+    var x = 10;
+    if (true) {
+        var x = 20; // mesma variável x é redeclarada dentro do bloco
+        console.log(x); // 20
+    }
+    console.log(x); // 20 (o valor foi alterado)
+}
+
+exemploVar();
